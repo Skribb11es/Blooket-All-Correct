196de7bfa20fd452c068a709307a70ff899c5dc3
Skribb11es <58915403+Skribb11es@users.noreply.github.com>
2022-11-21 01:21:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+Object.values(document.querySelector('#app>div>div'))[1].children[1]._owner.stateNode.questions = [{
+    "text": "github.com/skribb11es",
+    "answers": ["Who the hell","at blooket","thought it was a good idea","to handle answers on the client?"],
+    "correctAnswers": ["Who the hell","at blooket","thought it was a good idea","to handle answers on the client?"],
+}]
+Object.values(document.querySelector('#app>div>div'))[1].children[1]._owner.stateNode.freeQuestions = [{
+    "text": "github.com/skribb11es",
+    "answers": ["Who the hell","at blooket","thought it was a good idea","to handle answers on the client?"],
+    "correctAnswers": ["Who the hell","at blooket","thought it was a good idea","to handle answers on the client?"],
+}]
